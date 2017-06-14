5400f103d6346f5a1d128e5c03c8cd5c9e309c75
Washington Silva <w-osilva@hotmail.com>
2017-06-14 10:37:36 -0300

    Adicionando seed para usuário 'user'

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
-# Create admin user
+# Create admin and user
 
-unless User.find_by(email: 'admin@blog.com').present?
-  User.create(email: 'admin@blog.com', password: 'admin1234', nickname: 'Admin', first_name: 'Admin')
+%w( admin user ).each do |user|
+  email = "#{user}@blog.com"
+  unless User.find_by(email: email).present?
+    name = user.capitalize
+    password = "#{user}1234"
+    User.create(email: email, password: password, nickname: name, first_name: name)
+  end
 end
